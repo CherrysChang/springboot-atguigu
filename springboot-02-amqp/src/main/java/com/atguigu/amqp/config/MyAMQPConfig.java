e7e1a4ceb3f255a4627283f469fc3dbad812247b
CherrysChang <1935003650@qq.com>
2019-07-01 20:19:22 +0800

    修改默认序列化机制

--- springboot-02-amqp/src/main/java/com/atguigu/amqp/config/MyAMQPConfig.java
+++ springboot-02-amqp/src/main/java/com/atguigu/amqp/config/MyAMQPConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class MyAMQPConfig {
 
     @Bean
-    public MessageConverter messageConverter(){
-        return new Jackson2JsonMessageConverter();
+    public MessageConverter messageConverter(){//重新注册一个messageConverter。默认的就会失效，采用当前这个
+        return new Jackson2JsonMessageConverter();//使用json相关的实现类。具体实现可以对messageConverter接口进行ctrl+H查看
     }
 }
