360a3bbd64a7d6ac8ba7125b6f0eb7d4af356011
CherrysChang <1935003650@qq.com>
2019-07-01 19:52:56 +0800

    RabbitMQ整合

--- springboot-02-amqp/src/main/java/com/atguigu/amqp/Springboot02AmqpApplication.java
+++ springboot-02-amqp/src/main/java/com/atguigu/amqp/Springboot02AmqpApplication.java
@@ -7,12 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 自动配置
- *  1、RabbitAutoConfiguration
- *  2、有自动配置了连接工厂ConnectionFactory；
+ *  1、RabbitAutoConfiguration 自动配置类
+ *  2、有自动配置了连接工厂ConnectionFactory（连接消息代理）；
  *  3、RabbitProperties 封装了 RabbitMQ的配置
- *  4、 RabbitTemplate ：给RabbitMQ发送和接受消息；
- *  5、 AmqpAdmin ： RabbitMQ系统管理功能组件;
- *  	AmqpAdmin：创建和删除 Queue，Exchange，Binding
+ *  4、RabbitTemplate ：消息发送处理组件。给RabbitMQ发送和接受消息。
+ *  5、AmqpAdmin ： RabbitMQ系统管理功能组件;
+ *     AmqpAdmin：创建和删除 Queue（消息队列），Exchange（交换器），Binding（绑定消息队列和交换器之间的关联）
  *  6、@EnableRabbit +  @RabbitListener 监听消息队列的内容
  *
  */
