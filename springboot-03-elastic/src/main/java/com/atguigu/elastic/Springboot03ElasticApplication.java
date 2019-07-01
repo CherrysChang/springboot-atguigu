c602dbe6f7dfc82db1ba27e6117e9b0b8881da07
CherrysChang <1935003650@qq.com>
2019-07-01 23:23:58 +0800

    SpringBoot默认支持Jest、SpringData ElasticSearch）来和ES交互

--- springboot-03-elastic/src/main/java/com/atguigu/elastic/Springboot03ElasticApplication.java
+++ springboot-03-elastic/src/main/java/com/atguigu/elastic/Springboot03ElasticApplication.java
@@ -4,18 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * SpringBoot默认支持两种技术来和ES交互；
+ * SpringBoot默认支持两种技术来和ES（elasticsearch）交互；
  * 1、Jest（默认不生效）
- * 	需要导入jest的工具包（io.searchbox.client.JestClient）
+ * 	需要导入jest的工具包（io.searchbox.client.JestClient） 才会生效（JestAutoConfiguration自动配置类才会生效）
  * 2、SpringData ElasticSearch【ES版本有可能不合适】
  * 		版本适配说明：https://github.com/spring-projects/spring-data-elasticsearch
  *		如果版本不适配：2.4.6
  *			1）、升级SpringBoot版本
  *			2）、安装对应版本的ES
- *
+*	自动配置了如下信息：（ElasticsearchDataAutoCongiguration）		
  * 		1）、Client 节点信息clusterNodes；clusterName
  * 		2）、ElasticsearchTemplate 操作es
- *		3）、编写一个 ElasticsearchRepository 的子接口来操作ES；
+ *		3）、编写一个 ElasticsearchRepository 的子接口来操作ES；（类似于JPA的编程方式）
  *	两种用法：https://github.com/spring-projects/spring-data-elasticsearch
  *	1）、编写一个 ElasticsearchRepository
  */
