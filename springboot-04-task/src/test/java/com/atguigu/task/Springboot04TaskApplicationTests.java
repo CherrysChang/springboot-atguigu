76b19b28c116ffbb01fb53bd7fe9b682ed2db503
CherrysChang <1935003650@qq.com>
2019-07-02 21:15:43 +0800

    邮件任务执行，使用JavaMailSender

--- springboot-04-task/src/test/java/com/atguigu/task/Springboot04TaskApplicationTests.java
+++ springboot-04-task/src/test/java/com/atguigu/task/Springboot04TaskApplicationTests.java
@@ -20,27 +20,27 @@ public class Springboot04TaskApplicationTests {
 	JavaMailSenderImpl mailSender;
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() {//简单邮件发送
 		SimpleMailMessage message = new SimpleMailMessage();
 		//邮件设置
-		message.setSubject("通知-今晚开会");
-		message.setText("今晚7:30开会");
+		message.setSubject("通知-今晚开会");//邮件标题
+		message.setText("今晚7:30开会");//邮件内容
 
-		message.setTo("9******4@qq.com");
-		message.setFrom("y******h@126.com");
+		message.setTo("9******4@qq.com");//收件人邮箱
+		message.setFrom("y******h@126.com");//发件人
 
-		mailSender.send(message);
+		mailSender.send(message);//发送邮件
 	}
 
 	@Test
-	public void test02() throws  Exception{
+	public void test02() throws  Exception{//复杂邮件发送（比如带附件）
 		//1、创建一个复杂的消息邮件
 		MimeMessage mimeMessage = mailSender.createMimeMessage();
-		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);//如果需要上传文件，第二个参数multipart设为true
 
 		//邮件设置
 		helper.setSubject("通知-今晚开会-测试一下邮件发送");
-		helper.setText("<b style='color:red'>今天 7:30 开会</b>",true);
+		helper.setText("<b style='color:red'>今天 7:30 开会</b>",true);//内容可以是html片段，第二个参数html设为true，则可以编译html
 
 		helper.setTo("2******0@qq.com");
 		helper.setFrom("y******h@126.com");
