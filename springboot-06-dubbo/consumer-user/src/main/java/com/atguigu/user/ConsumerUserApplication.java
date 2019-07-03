86f82c2402abb050833a894135d8d1be888492b9
CherrysChang <1935003650@qq.com>
2019-07-03 11:37:27 +0800

    服务消费者 配置

--- springboot-06-dubbo/consumer-user/src/main/java/com/atguigu/user/ConsumerUserApplication.java
+++ springboot-06-dubbo/consumer-user/src/main/java/com/atguigu/user/ConsumerUserApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 1、引入依赖‘
- * 2、配置dubbo的注册中心地址
- * 3、引用服务
+ * 2、将服务消费者 注册到服务注册中心 
+ * 	1、引入dubbo和zkclient相关依赖
+ * 	2、配置dubbo的注册中心地址
+ * 	3、引用服务
  */
 @SpringBootApplication
 public class ConsumerUserApplication {
