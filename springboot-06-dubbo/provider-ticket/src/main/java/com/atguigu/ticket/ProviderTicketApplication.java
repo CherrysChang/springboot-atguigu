bb9aebf831cda9aaa411731b6a0d401e1948ead4
CherrysChang <1935003650@qq.com>
2019-07-03 11:23:12 +0800

    SpringBoot、Dubbo、Zookeeper整合，将服务提供者注册到注册中心

--- springboot-06-dubbo/provider-ticket/src/main/java/com/atguigu/ticket/ProviderTicketApplication.java
+++ springboot-06-dubbo/provider-ticket/src/main/java/com/atguigu/ticket/ProviderTicketApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 1、将服务提供者注册到注册中心
- * 	    1、引入dubbo和zkclient相关依赖
- * 	    2、配置dubbo的扫描包和注册中心地址
- * 	    3、使用@Service发布服务
+ * 步骤：1、引入dubbo和zkclient相关依赖（这里引入dubbo-spring-boot-starter和zookeeper的客户端工具zkclient）
+ * 	2、配置dubbo的扫描包和注册中心地址（在application.properties配置）
+ * 	3、使用@Service发布服务（@Service这个注解是dubbo包下的）
+ 
+ * ps：zookeeper作为服务注册中心，dubbo作为分布式服务调用框架（比如负责A模块和B模块远程过程调用）
  */
 @SpringBootApplication
 public class ProviderTicketApplication {
