1058a2b03d60769d10f46726c55c82b5dce70e75
CherrysChang <1935003650@qq.com>
2019-07-03 13:22:58 +0800

    服务提供者的功能部分

--- springboot-06-springcloud/provider-ticket/src/main/java/com/atguigu/providerticket/controller/TicketController.java
+++ springboot-06-springcloud/provider-ticket/src/main/java/com/atguigu/providerticket/controller/TicketController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//SpringCloud在整合微服务时是通过轻量级http进行通信的。
 @RestController
 public class TicketController {
 
@@ -13,7 +14,7 @@ public class TicketController {
     TicketService ticketService;
 
     @GetMapping("/ticket")
-    public String getTicket(){
+    public String getTicket(){//这里通过http的形式将服务接口暴露出来
         return ticketService.getTicket();
     }
 }
