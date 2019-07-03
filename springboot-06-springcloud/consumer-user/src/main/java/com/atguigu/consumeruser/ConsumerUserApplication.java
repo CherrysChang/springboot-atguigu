0e8c7c007881cb68336fe20da50dca8f2a8fabd4
CherrysChang <1935003650@qq.com>
2019-07-03 13:57:12 +0800

    服务发现 & 消费

--- springboot-06-springcloud/consumer-user/src/main/java/com/atguigu/consumeruser/ConsumerUserApplication.java
+++ springboot-06-springcloud/consumer-user/src/main/java/com/atguigu/consumeruser/ConsumerUserApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableDiscoveryClient //开启发现服务功能
+@EnableDiscoveryClient //开启发现服务功能。（这样就可以从Eureka服务注册中心获取到其他服务，并能调用）
 @SpringBootApplication
 public class ConsumerUserApplication {
 
@@ -15,9 +15,9 @@ public class ConsumerUserApplication {
 		SpringApplication.run(ConsumerUserApplication.class, args);
 	}
 
-	@LoadBalanced //使用负载均衡机制
+	@LoadBalanced //使用负载均衡机制 （比如我们启动了服务提供者应用的多个实例，访问哪个服务采用轮询的方式）
 	@Bean
-	public RestTemplate restTemplate(){
+	public RestTemplate restTemplate(){//注册restTemplate，帮我们发送http请求
 		return new RestTemplate();
 	}
 }
