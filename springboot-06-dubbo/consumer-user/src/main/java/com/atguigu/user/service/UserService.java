ecd5595dfe7f9a244e7e2f0f4f024aa25601c692
CherrysChang <1935003650@qq.com>
2019-07-03 11:37:47 +0800

    服务消费者 引用服务

--- springboot-06-dubbo/consumer-user/src/main/java/com/atguigu/user/service/UserService.java
+++ springboot-06-dubbo/consumer-user/src/main/java/com/atguigu/user/service/UserService.java
@@ -5,10 +5,10 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.atguigu.ticket.service.TicketService;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service//这个是Spring的注解，不是dubbo的
 public class UserService{
 
-    @Reference
+    @Reference //dubbo注解，远程引用（发布时按照对应接口的全类名发布的，这边引用就可以获取到。前提这个要跟那个发布的全类名一致）
     TicketService ticketService;
 
     public void hello(){
